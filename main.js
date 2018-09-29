7a86265c7d3179fbcdaff404b10d3bf75c769ab0
sahan <sahan.lamahewa@gmail.com>
2018-09-30 04:40:19 +0530

    dddd

--- main.js
+++ main.js
@@ -34,8 +34,41 @@ $.ajax ({
     success: function (res){
       console.log(res);
       //console.log("ICE List: "+res.v.iceServers);
-      customConfig =res.v.iceServers;
-      console.log(customConfig);
+      customConfig = {{url:'stun:stun01.sipphone.com'},
+{url:'stun:stun.ekiga.net'},
+{url:'stun:stun.fwdnet.net'},
+{url:'stun:stun.ideasip.com'},
+{url:'stun:stun.iptel.org'},
+{url:'stun:stun.rixtelecom.se'},
+{url:'stun:stun.schlund.de'},
+{url:'stun:stun.l.google.com:19302'},
+{url:'stun:stun1.l.google.com:19302'},
+{url:'stun:stun2.l.google.com:19302'},
+{url:'stun:stun3.l.google.com:19302'},
+{url:'stun:stun4.l.google.com:19302'},
+{url:'stun:stunserver.org'},
+{url:'stun:stun.softjoys.com'},
+{url:'stun:stun.voiparound.com'},
+{url:'stun:stun.voipbuster.com'},
+{url:'stun:stun.voipstunt.com'},
+{url:'stun:stun.voxgratia.org'},
+{url:'stun:stun.xten.com'},
+{
+	url: 'turn:numb.viagenie.ca',
+	credential: 'muazkh',
+	username: 'webrtc@live.com'
+},
+{
+	url: 'turn:192.158.29.39:3478?transport=udp',
+	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	username: '28224511:1379330808'
+},
+{
+	url: 'turn:192.158.29.39:3478?transport=tcp',
+	credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+	username: '28224511:1379330808'
+}};
+      //console.log(customConfig);
     }
 });
 
@@ -63,7 +96,7 @@ socket.on('DANG_KY_THAT_BAT', () => alert('Vui long chon username khac!'));
 
 
 function openStream() {
-    const config = { audio: true, video: true };
+    const config = { audio: false, video: true };
     return navigator.mediaDevices.getUserMedia(config);
 }
 
